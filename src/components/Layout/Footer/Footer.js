94c5f6c6dcf4b5bad710d5bf6749a7ee6a08902c
nerisina <neri.esparza@gmail.com>
2020-08-22 16:19:25 -0400

    Active class

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 //Assets
 import AProjectbyPastel from "../../../assets/AProjectbyPastel.svg";
-
 import Ad from './Ad'
+
 const footer = (props) => {
+
   return (
     <footer>
     {props.formIsSubmited ? (
@@ -13,12 +14,12 @@ const footer = (props) => {
         <img src={AProjectbyPastel} alt="copy" />
       </div>
     ) : (
-      <div className="bottom__nav">
-        <div className="bottom__nav--right">
-            <Link className="bottom__nav--btn" to="/design">Design</Link>
-            <Link className="bottom__nav--btn" to="/website">Website</Link>
+      <div className="nav">
+        <div className="nav__left">
+            <NavLink activeClassName="nav__link--active" className="nav__link" exact to="/design">Design</NavLink>
+            <NavLink activeClassName="nav__link--active" className="nav__link" to="/website">Website</NavLink>
         </div>
-        <div className="bottom__nav--left">
+        <div className="nav__right">
           <Ad />
         </div>
       </div>
